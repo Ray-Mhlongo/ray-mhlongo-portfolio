106fc8d0da21733e705ccc1581334876fd2a4dba
Ray-Mhlongo <rodgersmhlongo@gmail.com>
2026-05-27 21:44:59 +0200

    Update ray-ai-config.js

--- ray-ai-config.js
+++ ray-ai-config.js
@@ -1,5 +1,5 @@
 window.RAY_AI_CONFIG = {
-  provider: "groq",
-  apiKey: "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",
-  model: "llama-3.1-8b-instant"
+  provider: "Gemini",
+  apiKey: "AIzaSyBCtUhZvxB5DIvenwHg5shBt6QbajSrRKY",
+  model: "gemini-1.5-flash"
 };
